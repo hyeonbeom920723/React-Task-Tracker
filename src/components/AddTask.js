7886f4e4bfde27f9f8b0427089aae908501dbf73
Hyeonbeom <hyeonbeom920723@gmail.com>
2021-07-27 14:05:42 -0600

    First commit

---
+++ src/components/AddTask.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text) {
+      alert("Please Add a task");
+      return;
+    }
+
+    onAdd({ text, day, reminder });
+
+    // clear
+    setText("");
+    setDay("");
+    setReminder(false);
+  };
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => {
+            return setText(e.target.value);
+          }}
+        />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input
+          type="text"
+          placeholder="Add Day & Time"
+          value={day}
+          onChange={(e) => {
+            return setDay(e.target.value);
+          }}
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => {
+            return setReminder(e.currentTarget.checked);
+          }}
+        />
+      </div>
+      <input type="submit" value="Save task" className="btn btn-block" />
+    </form>
+  );
+};
+
+export default AddTask;
